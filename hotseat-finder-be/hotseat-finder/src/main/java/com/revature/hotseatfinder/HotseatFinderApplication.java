1bcaf7d6108c8fbaa5e3c5276ce512979b91cb38
Richard Vo <richard.vobusiness@gmail.com>
2020-03-23 13:15:42 -0500

    added be

---
+++ hotseat-finder-be/hotseat-finder/src/main/java/com/revature/hotseatfinder/HotseatFinderApplication.java
@@ -0,0 +1,13 @@
+package com.revature.hotseatfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotseatFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotseatFinderApplication.class, args);
+	}
+
+}
